1547e400ecdba74af37f6544ee76df22dd0041ec
Samir Faycal Tahar M'Sallem <samir.faycal.tahar.m.sallem@mni.thm.de>
2022-06-16 13:13:38 +0200

    firebase dependency and serve static angular via backend

--- my-cargonaut-backend1/src/app.module.ts
+++ my-cargonaut-backend1/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: `${__dirname}/../../my-cargonaut-frontend/dist/my-cargonaut-frontend`,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
