4056a7d7eace40ce05a263a86aeabfad9cac2218
vladi <vladislav.bova@mni.thm.de>
2022-06-20 23:49:26 +0200

    login and register component styling with bootstrap and css

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
 import {RouterModule} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
 import { LoginComponent } from './components/login/login.component';
 import { ProfilComponent } from './components/profil/profil.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
