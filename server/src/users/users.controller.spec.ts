b87521a3e5a0640689c39cd5f73dddfe4a6cc347
Samir Faycal Tahar M'Sallem <samir.faycal.tahar.m.sallem@mni.thm.de>
2022-06-21 16:05:58 +0200

    fix backend unit tests

--- server/src/users/users.controller.spec.ts
+++ server/src/users/users.controller.spec.ts
@@ -1,12 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import {UsersService} from "./users.service";
+import {getModelToken} from "@nestjs/mongoose";
+import {User} from "./user.model";
 
 describe('UsersController', () => {
   let controller: UsersController;
 
+  const mockUser = (
+      _id = '1',
+      firstname = 'Test',
+      lastname = 'Test',
+      email = 'Test',
+      password  = 'Test',
+      description = 'Test',
+  ): User => <User>({
+    _id,
+    firstname,
+    lastname,
+    email,
+    password,
+    description,
+  });
+
+
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken('User'),
+          useValue: {
+            new: jest.fn().mockResolvedValue(mockUser()),
+            constructor: jest.fn().mockResolvedValue(mockUser()),
+            find: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            create: jest.fn(),
+            remove: jest.fn(),
+            exec: jest.fn(),
+          },
+        }
+          ]
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
--- server/src/users/users.service.spec.ts
+++ server/src/users/users.service.spec.ts
@@ -1,12 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import {User} from "./user.model";
+import {UsersController} from "./users.controller";
+import {getModelToken} from "@nestjs/mongoose";
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  const mockUser = (
+      _id = '1',
+      firstname = 'Test',
+      lastname = 'Test',
+      email = 'Test',
+      password  = 'Test',
+      description = 'Test',
+  ): User => <User>({
+    _id,
+    firstname,
+    lastname,
+    email,
+    password,
+    description,
+  });
+
+
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken('User'),
+          useValue: {
+            new: jest.fn().mockResolvedValue(mockUser()),
+            constructor: jest.fn().mockResolvedValue(mockUser()),
+            find: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            create: jest.fn(),
+            remove: jest.fn(),
+            exec: jest.fn(),
+          },
+        }
+      ]
     }).compile();
 
     service = module.get<UsersService>(UsersService);
