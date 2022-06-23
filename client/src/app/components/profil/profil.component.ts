d910e1e8878579bd55dd47a09a53d3fa949d9f84
Samir Faycal Tahar M'Sallem <samir.faycal.tahar.m.sallem@mni.thm.de>
2022-06-23 23:13:24 +0200

    several fixes with login and register and routing

--- client/src/app/components/profil/profil.component.ts
+++ client/src/app/components/profil/profil.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from "../../service/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-profil',
@@ -8,7 +9,11 @@ import {UserService} from "../../service/user.service";
 })
 export class ProfilComponent implements OnInit {
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private _router: Router) {
+    if (localStorage.getItem('authenticated') == 'false') {
+      this._router.navigate([''])
+    }
+  }
 
   currentUser: any;
   email = '';
